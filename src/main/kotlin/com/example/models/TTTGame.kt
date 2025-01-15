58068db8c2e253bd5dc69645f23d598d97606b16
vicki <surajbarik2003@gmail.com>
2025-01-16 03:23:43 +0530

    implemented all the required code for game engine to work

--- src/main/kotlin/com/example/models/TTTGame.kt
+++ src/main/kotlin/com/example/models/TTTGame.kt
@@ -1,11 +1,13 @@
 package com.example.models
 
 import io.ktor.websocket.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import java.util.concurrent.ConcurrentHashMap
 
 class TTTGame {
@@ -15,6 +17,15 @@ class TTTGame {
     private val playerSockets = ConcurrentHashMap<Char, WebSocketSession>()
 
     private val gameScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    private var delayGameJob: Job? = null
+
+    init {
+        // on each generation we call broadcast func
+        // i.e, whenever our state changes we call our broadcast func with that corresponding state
+        //  gameScope is coroutine where this broadcast is running
+        state.onEach(::broadcast).launchIn(gameScope)
+    }
 
     fun connectPlayer(session: WebSocketSession): Char? {
         val isPlayerA = state.value.connectedPlayers.any { it == 'X' }
@@ -42,5 +53,74 @@ class TTTGame {
     }
 
     // fun to send game state to players
-    fun broadcast()
+    suspend fun broadcast(state: GameState) {
+        playerSockets.values.forEach { socket ->
+            socket.send(
+                Json.encodeToString(state)
+            )
+        }
+    }
+
+    fun finishTurn(player: Char, x: Int, y: Int) {
+        if (state.value.field[y][x] != null  ||  state.value.winningPlayer != null) return
+
+        if (state.value.playerAtTurn != player) return
+
+        val currentPlayer = state.value.playerAtTurn
+        state.update {
+            // updated the specific field to the player that make that turn and saved it to newField
+            val newField = it.field.also { field ->
+                field[y][x] = currentPlayer
+            }
+            // if all field in newField is not null so board is full
+            val isBoardFull = newField.all { it != null }
+            if (isBoardFull) {
+                startNewRoundDelay()
+            }
+            it.copy(
+                playerAtTurn = if (currentPlayer == 'X') 'O' else 'X',
+                field = newField,
+                isBoardFull = isBoardFull,
+                winningPlayer = getWinningPlayer()?.also {
+                    startNewRoundDelay()
+                }
+            )
+        }
+    }
+
+    private fun getWinningPlayer(): Char? {
+        val filed = state.value.field
+        return if (filed[0][0] != null && filed[0][0] == filed[0][1] && filed[0][1] == filed[0][2]) {
+            filed[0][0]
+        } else if (filed[1][0] != null && filed[1][0] == filed[1][1] && filed[1][1] == filed[1][2]) {
+            filed[1][0]
+        } else if (filed[2][0] != null && filed[2][0] == filed[2][1] && filed[2][1] == filed[2][2]) {
+            filed[2][0]
+        } else if (filed[0][0] != null && filed[0][0] == filed[1][0] && filed[1][0] == filed[2][0]) {
+            filed[0][0]
+        } else if (filed[0][1] != null && filed[0][1] == filed[1][1] && filed[1][1] == filed[2][1]) {
+            filed[0][1]
+        } else if (filed[0][2] != null && filed[0][2] == filed[1][2] && filed[1][2] == filed[2][2]) {
+            filed[0][2]
+        } else if (filed[0][0] != null && filed[0][0] == filed[1][1] && filed[1][1] == filed[2][2]) {
+            filed[0][0]
+        } else if (filed[0][2] != null && filed[0][2] == filed[1][1] && filed[1][1] == filed[2][0]) {
+            filed[0][2]
+        } else null
+    }
+
+    private fun startNewRoundDelay() {
+        delayGameJob?.cancel()
+        delayGameJob = gameScope.launch {
+            delay(5000L)
+            state.update {
+                it.copy(
+                    playerAtTurn = 'X',
+                    field = GameState.emptyField(),
+                    winningPlayer = null,
+                    isBoardFull = false 
+                )
+            }
+        }
+    }
 }
